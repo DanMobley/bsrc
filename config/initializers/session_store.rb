3cd153df839d571f58ca85e2c92c705655aa2b14
Remo Mueller <remosm@gmail.com>
2015-06-08 13:48:42 -0400

    Make sure session store only uses secure in production

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_bsrc_session'
+Rails.application.config.session_store :cookie_store, key: '_bsrc_session',
+                                                   secure: Rails.env.production?
